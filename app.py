8ce4fa9da5618989bf85d633219b73691f17b4ff
f4zolli <fazolli_matheus@hotmail.com>
2022-12-24 22:16:29 -0300

    feat: initial flask restful project

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Users(Resource):
+    def get(self):
+        return {"message": "user1"}
+
+class User(Resource):
+    def post(self):
+        return {"message": "teste"}
+    def get(self, cpf):
+        return {"message": "CPF"}
+
+api.add_resource(Users, '/users')
+api.add_resource(User, '/user', '/user/<string:cpf>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
